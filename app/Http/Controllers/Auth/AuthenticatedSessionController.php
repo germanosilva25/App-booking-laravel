7ed7c7c1cdb34a1a14b6a731273de74932134c5f
germanosilva25 <germanosilva@yahoo.com.br>
2025-08-08 21:20:04 -0300

    décima oitava

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -1,4 +1,16 @@
 <?php
+namespace App\Http\Controllers\Auth;
 
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use League\OAuth2\Client\Provider\Google;
+
+class AuthenticatedSessionController extends Controller
+{
+
+}
 
 ?>
